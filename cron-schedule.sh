a29864b024f8e3854f8afffff61c98f4bdad147d
softracegit <101907772+softracegit@users.noreply.github.com>
2026-05-12 00:49:39 +0100

    cron

---
+++ cron-schedule.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cd /home/u291405135/domains/softrace.pt/public_html/beauty && /opt/alt/php82/usr/bin/php artisan schedule:run >> /home/u291405135/domains/softrace.pt/public_html/beauty/cron.log 2>&1
